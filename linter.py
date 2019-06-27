866e637a70d0fcef503b801e19819dc74db23d3f
Denis Krienbühl <denis@href.ch>
2019-06-27 11:40:18 +0200

    Define basic elvish linter

--- linter.py
+++ linter.py
@@ -1,10 +1,23 @@
-from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+from SublimeLinter.lint import Linter
+from SublimeLinter.lint.util import STREAM_STDERR
 
 
-class __class__(Linter):
-    cmd = '__cmd__'
-    regex = r''
-    multiline = False
+class Elvish(Linter):
+    cmd = 'elvish -compileonly -norc'
+
+    regex = (
+        r"Compilation error: (?P<message>[^\n]+)\n"
+        r".*\.elv, line (?P<line>[0-9]+)"
+    )
+
+    multiline = True
+
+    # Elvish is capable of compiling from stdin, but it'll ignore the last
+    # line if it doesn't end with a newline. Using a file works.
+    tempfile_suffix = '.elv'
+
+    error_stream = STREAM_STDERR
+
     defaults = {
-        'selector': 'source.python'
+        'selector': 'source.elvish'
     }
